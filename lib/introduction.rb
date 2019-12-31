87197380f5de74f16a91f8e12a6c7a5ccefd1a5b
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-31 08:07:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}, I am a #{language} programmer."
+  puts "Hi, my name is #{name} andI am learning to program #{language}."
 end
